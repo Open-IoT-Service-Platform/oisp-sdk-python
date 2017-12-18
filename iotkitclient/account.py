08701d445ef3e7eb986dc74956bf6552788f3968
Ali Rasim Kocal <arkocal@gmail.com>
2017-12-18 11:14:36 +0100

    Refactoring according to pylint and pycodestyle

--- iotkitclient/account.py
+++ iotkitclient/account.py
@@ -1,43 +1,46 @@
-# Copyright (c) 2015, Intel Corporation
+# Copyright (c) 2017, Intel Corporation
 #
-# Redistribution and use in source and binary forms, with or without modification,
-# are permitted provided that the following conditions are met:
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
 #
 #    * Redistributions of source code must retain the above copyright notice,
 #      this list of conditions and the following disclaimer.
-#    * Redistributions in binary form must reproduce the above copyright notice,
-#      this list of conditions and the following disclaimer in the documentation
-#      and/or other materials provided with the distribution.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
 #    * Neither the name of Intel Corporation nor the names of its contributors
 #      may be used to endorse or promote products derived from this software
 #      without specific prior written permission.
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-"""@package Account
-Methods for IoT Analytics account management
+"""
+Methods for IoT Analytics account management.
 """
 from device import Device
 
 
-class Account(object): #TODO document attributes
-    """ Create IoT Account instance
-
-    """
+# pylint: disable=too-many-instance-attributes
+# The attributes match those defined in the REST API
+class Account(object):
+    """Create IoT Account instance."""
 
     ROLE_ADMIN = "admin"
     ROLE_USER = "user"
 
     def __init__(self, client, name, account_id, role):
+        """Create account object."""
         self.client = client
         self.name = name
         self.account_id = account_id
@@ -45,6 +48,7 @@ class Account(object): #TODO document attributes
         self.devices = None
         self.activation_code = None
         self.activation_code_valid_until = None
+        # TODO rename this to url for consistency among project
         self.account_url = "/accounts/{}".format(self.account_id)
 
     def __str__(self):
@@ -52,47 +56,46 @@ class Account(object): #TODO document attributes
                                                            self.account_id,
                                                            self.role)
 
-
     def delete(self):
-        """ Delete account"""
+        """Delete account."""
         self.client.delete(self.account_url, expect=204)
-        #TODO inform client
-
+        # TODO inform client
 
     def get_activation_code(self, auto_refresh=True):
-        """ Return previous activation code a string if it is still valid.
+        """Return previous activation code a string if it is still valid.
 
         If auto_refresh is True (default), this will automatically
         request a new activation code if the old one is expired.
         Otherwise it will return None
+
         """
         endpoint = self.account_url + "/activationcode"
         resp = self.client.get(endpoint, expect=200)
         activation_code = resp.json()["activationCode"]
-        #time_left = resp.json()["timeLeft"]
+        # time_left = resp.json()["timeLeft"]
         if not activation_code and auto_refresh:
             activation_code = self.refresh_activation_code()
         self.activation_code = activation_code
-        self.activation_code_valid_until = None #TODO check for validation
+        self.activation_code_valid_until = None  # TODO check for validation
         return activation_code
 
-
     def refresh_activation_code(self):
-        """ Request a new activation code from server.
+        """Request a new activation code from server.
 
-        Returns the activation code as string."""
+        Returns the activation code as string.
+
+        """
         endpoint = self.account_url + "/activationcode/refresh"
         resp = self.client.put(endpoint, expect=200)
         self.activation_code = resp.json()["activationCode"]
         return self.activation_code
-
 
     # pylint: disable=unused-argument, too-many-arguments
     # Arguments are accessed via locals() and as many as API parameters are
     # necessary
     def get_devices(self, sort=None, order=None, limit=None, skip=None,
                     device_id=None, gateway_id=None, name=None, status=None):
-        """ Get a list of devices connected to the account.
+        """Get a list of devices connected to the account.
 
         Args
         ----------
@@ -104,6 +107,7 @@ class Account(object): #TODO document attributes
         gateway_id (str): Filter by gateway id
         name (str): Filter by name
         status (str): Filter by status
+
         """
         endpoint = self.account_url + "/devices"
         endpoint += "&".join(["{}={}".format(param, locals()[param])
@@ -113,28 +117,29 @@ class Account(object): #TODO document attributes
                               if locals()[param] is not None])
 
         resp = self.client.get(endpoint, expect=200)
-        self.devices = [] # TODO decide whether old devices should be kept
+        self.devices = []  # TODO decide whether old devices should be kept
         for device_json in resp.json():
             self.devices.append(Device.from_json(self, device_json))
         return self.devices
 
-
     def get_device(self, device_id):
-        """ Get device with given id. """
+        """Get device with given id."""
         endpoint = self.account_url + "/devices/" + device_id
         resp = self.client.get(endpoint, 200)
         return Device.from_json(self, resp.json())
 
-
     def create_device(self, device_id, name, gateway_id=None, tags=None,
                       loc=None, attributes=None):
-        """ Create a device
+        """Create a device.
 
-        Device ID has to be unique. """
+        Device ID has to be unique.
+
+        """
         endpoint = "/accounts/{}/devices".format(self.account_id)
         if gateway_id is None:
             gateway_id = device_id
-        payload = {"deviceId": device_id, "gatewayId": gateway_id, "name":name}
+        payload = {"deviceId": device_id,
+                   "gatewayId": gateway_id, "name": name}
         if tags:
             payload["tags"] = tags
         if loc:
@@ -145,23 +150,22 @@ class Account(object): #TODO document attributes
 
         return Device.from_json(self, resp.json())
 
-
     def get_device_tags(self):
-        """ Return a list of all device tags. """
+        """Return a list of all device tags."""
         endpoint = self.account_url + "/devices/tags"
         resp = self.client.get(endpoint, expect=200)
         return resp.json()
 
-
     def get_device_attributes(self):
-        """ Return a dictionary of all device attributes
+        """Return a dictionary of all device attributes.
 
-        Each entry contains a list for all values for an attribute"""
+        Each entry contains a list for all values for an attribute
+
+        """
         endpoint = self.account_url + "/devices/attributes"
         resp = self.client.get(endpoint, expect=200)
         return resp.json()
 
-
-    def get_component_types(self): #TODO OOP
-        endpoint = "/accounts/{}/cmpcatalog?full=true".format(self.account_id)
-        return self.client.get(endpoint)
+#    def get_component_types(self):  # TODO OOP
+#        endpoint = "/accounts/{}/cmpcatalog?full=true".format(self.account_id)
+#        return self.client.get(endpoint)
--- iotkitclient/client.py
+++ iotkitclient/client.py
@@ -1,31 +1,32 @@
-# Copyright (c) 2017, Intel Corporation
+# Copyright (c) 2015, Intel Corporation
 #
-# Redistribution and use in source and binary forms, with or without modification,
-# are permitted provided that the following conditions are met:
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
 #
 #    * Redistributions of source code must retain the above copyright notice,
 #      this list of conditions and the following disclaimer.
-#    * Redistributions in binary form must reproduce the above copyright notice,
-#      this list of conditions and the following disclaimer in the documentation
-#      and/or other materials provided with the distribution.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
 #    * Neither the name of Intel Corporation nor the names of its contributors
 #      may be used to endorse or promote products derived from this software
 #      without specific prior written permission.
 #
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-"""@package Connect
-Methods for IoT Analytics Cloud connections
-
+"""
+Methods for IoT Analytics Cloud connections.
 """
 import json
 
@@ -37,20 +38,28 @@ from oic_user import User
 
 
 class AuthenticationError(Exception):
-    """Authentication Error class for Open IOT Connector
+    """Authentication Error class for Open IOT Connector.
 
-    This Error is thrown if an error occurs before
-    server is even contacted, otherwise an OIC Exception
-    will be thrown, even in the case of an authentication
-    related exception"""
+    This Error is thrown if an error occurs before server is even
+    contacted, otherwise an OIC Exception will be thrown, even in the
+    case of an authentication related exception
+
+    """
+
     pass
 
 
 class OICException(Exception):
-    """Exception for Open IOT Connector, for cases when an
-    error code is returned from the server."""
+    """Exception for cases when an error code is returned from the server."""
 
     def __init__(self, expect, resp):
+        """ Create OICException.
+
+        Args
+        ----------
+        expect: Expected HTTP Response code
+        resp: Received response object from requests.
+        """
         message = ("Exception during API call\n"
                    "Status code: {}, {} was expected".format(resp.status_code,
                                                              expect))
@@ -61,19 +70,17 @@ class OICException(Exception):
         super(OICException, self).__init__(message)
 
 
-
 class Client(object):
-    """ IoT Analytics Cloud client class
+    """IoT Analytics Cloud client class.
 
-    Attributes:
-      proxies (str): proxy server used for connection
-      user_token (str): access token from IoT Analytics site connection
-      user_id (str): user ID for authenticated user
+    Attributes:   proxies (str): proxy server used for connection
+    user_token (str): access token from IoT Analytics site connection
+    user_id (str): user ID for authenticated user
+
     """
 
     def __init__(self, api_root, proxies=None, verify_certs=True):
-        """ Create IoT Analytics user session, set up connection
-        information (host, proxy connections) and test the connection.
+        """Set up IOT Analytics Cloud connection.
 
         Args:
         ----------
@@ -84,6 +91,7 @@ class Client(object):
         The API will respect system proxy settings if none specified.
         verify_certs (bool, optional): Whether the certificates should
         be verified on each request.
+
         """
         self.base_url = api_root
         self.proxies = proxies
@@ -93,13 +101,13 @@ class Client(object):
         # Test connection
         self.get_server_info()
 
-
     def get_headers(self, authorize=True):
-        """ Return a JSON dictionary containing request headers.
+        """Return a JSON dictionary containing request headers.
 
         Args:
         ---------
         authorized (bool, optional): Whether user token is to be included
+
         """
         headers = {"content-type": "application/json"}
         if not authorize:
@@ -114,9 +122,8 @@ class Client(object):
         headers["Authorization"] = "Bearer " + self.user_token.value
         return headers
 
-
     def auth(self, username, password):
-        """ Submit IoT Analytics user credentials to obtain the access token
+        """Submit IoT Analytics user credentials to obtain the access token.
 
         Sets user_id and user_token attributes for connection instance
 
@@ -124,6 +131,7 @@ class Client(object):
         ----------
         username (str): username for IoT Analytics site
         password (str): password for IoT Analytics site
+
         """
         payload = {"username": username, "password": password}
         resp = self.post("/auth/token", data=payload, authorize=False,
@@ -133,14 +141,14 @@ class Client(object):
         self.user_token = self.get_user_token(token_str)
         self.user_id = self.user_token.user_id
 
-
     def get_user_token(self, token_str=None):
-        """ Return a UserToken object containing user token information.
+        """Return a UserToken object containing user token information.
 
         Args:
         ----------
         token_str (str): If token string is not specified, the
         last acquired token will be used.
+
         """
         if not token_str and self.user_token:
             return self.user_token
@@ -157,74 +165,72 @@ class Client(object):
                         expect=200)
         return UserToken.from_json(token_str, resp.json(), client=self)
 
-
     def get_user(self, user_id=None):
-        """ Get the user with given user_id. If None specified, the token
-        holder will be returned."""
+        """Get the user with given user_id.
+
+        If None specified, the token holder will be returned.
+
+        """
         if not user_id:
             user_id = self.user_token.user_id
         resp = self.get("/users/" + user_id, expect=200)
         return User.from_json(client=self, json_dict=resp.json())
 
-
     def reset_password_request_mail(self, email):
-        """ Send a password reset mail to given email adress. """
-        self.post("/users/forgot_password", data={"email":email},
+        """Send a password reset mail to given email adress."""
+        self.post("/users/forgot_password", data={"email": email},
                   authorize=False, expect=200)
 
-
     def reset_password_submit_new(self, token, password):
-        """ Reset password using the token obtained via email. """
-        payload = {"token":token, "password":password}
+        """Reset password using the token obtained via email."""
+        payload = {"token": token, "password": password}
         self.put("/users/forgot_password", data=payload,
                  authorize=False, expect=200)
 
-
     def change_user_password(self, email, current_password, new_password):
-        """ Change password for user identified by email. """
+        """Change password for user identified by email."""
         url = "/users/{}/change_password".format(email)
-        payload = {"currentpwd":current_password, "password":new_password}
+        payload = {"currentpwd": current_password, "password": new_password}
         self.put(url, data=payload, authorize=False, expect=200)
 
-
     def request_user_activation(self, email):
-        """ Send user with given email adress an activation mail."""
-        self.post("/users/request_user_activation", data={"email":email},
+        """Send user with given email adress an activation mail."""
+        self.post("/users/request_user_activation", data={"email": email},
                   authorize=False, expect=200)
 
-
     def get_server_info(self):
-        """ Get cloud version and health information
+        """Get cloud version and health information.
 
         Returns: a JSON dictionary
+
         """
         resp = self.get("/health", authorize=False, expect=200)
         return resp.json()
 
-
     def get_accounts(self):
-        """ Get a list of accounts connected to current
-        authentication token. """
+        """Get a list of accounts connected to current authentication token."""
         return self.user_token.accounts
 
-
     def create_account(self, name):
-        """ Create an account with given name and return an Account instance.
+        """Create an account with given name and return an Account instance.
 
-        A new token needs to be acquired using the auth method to access the
-        account. """
+        A new token needs to be acquired using the auth method to access
+        the account.
+
+        """
         payload = {"name": name}
         resp = self.post("/accounts", data=payload, expect=201)
         resp_json = resp.json()
         return Account(self, resp_json["name"], resp_json["id"],
                        Account.ROLE_ADMIN)
 
-
     def _make_request(self, request_func, endpoint, authorize, expect=None,
                       *args, **kwargs):
-        """ Make a request using global settings and raise an OICException
-        if a status code other than expect is returned """
+        """Make a request using global settings.
 
+        Raises an OICException if a status code other than expect
+        is returned.
+        """
         headers = kwargs.pop("headers", self.get_headers(authorize=authorize))
         proxies = kwargs.pop("proxies", self.proxies)
         verify = kwargs.pop("verify", self.verify_certs)
@@ -239,6 +245,7 @@ class Client(object):
 
     def get(self, endpoint, authorize=True, *args, **kwargs):
         """ Make a GET request.
+
         Args:
         ----------
         endpoint: Endpoint without the API root.
@@ -250,6 +257,7 @@ class Client(object):
 
     def post(self, endpoint, authorize=True, *args, **kwargs):
         """ Make a POST request.
+
         Args:
         ----------
         endpoint: Endpoint without the API root.
@@ -261,6 +269,7 @@ class Client(object):
 
     def put(self, endpoint, authorize=True, *args, **kwargs):
         """ Make a PUT request.
+
         Args:
         ----------
         endpoint: Endpoint without the API root.
@@ -272,6 +281,7 @@ class Client(object):
 
     def delete(self, endpoint, authorize=True, *args, **kwargs):
         """ Make a DELETE request.
+
         Args:
         ----------
         endpoint: Endpoint without the API root.
